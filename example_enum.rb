5f06fb66ce8922dc84a502f165230dc7ea9d6a20
Will <whp3652@rit.edu>
2013-11-17 18:25:24 +0000

    update enum example

--- example_enum.rb
+++ example_enum.rb
@@ -20,6 +20,7 @@ end
 # The block is passed a counter as a parameter.
 # Calling the "each" method with a block looks like this:
 
+puts "Loop through a range"
 (1..5).each do |counter|
   puts "iteration #{counter}"
 end
@@ -29,6 +30,7 @@ end
 #=> iteration 4
 #=> iteration 5
 
+puts "Loop through a range with a block"
 # You can also surround blocks in curly brackets:
 (1..5).each {|counter| puts "iteration #{counter}"}
 
@@ -37,10 +39,18 @@ end
 # The contents of data structures can also be iterated using each.
 array = [1, 2, 3, 4, 5]
 
+puts "Loop through array"
 array.each do |element|
   puts "#{element} is part of the array"
 end
 
+# If you want the index as well use each_with_index
+puts "Loop through array with an index"
+array.each_with_index do |element, index|
+  puts "#{element} is in the #{index} slot of the array"
+end
+
+puts "Loop through Hash:"
 hash = { color: 'green', number: 5}
 
 hash.each do |key, value|
@@ -49,6 +59,7 @@ end
 
 # There are while loops too
 
+puts "While loop:"
 counter = 1
 while counter <= 5 do
   puts "iteration #{counter}"
