2341b94962009495b87966ca88e074005efab321
Will <whp3652@rit.edu>
2013-11-17 06:55:55 +0000

    switch to choices to symbols for better performance

--- example_app.rb
+++ example_app.rb
@@ -8,34 +8,35 @@ class Inventory
 	def add_item(name, author)
 		temp_item = Item.new(name,author)
 		@items_array << temp_item
+    puts 'Added Item'
 	end
 
 	def display_inventory
 		@items_array.each do |item|
-			puts "Item named: #{item.name} \t Made by: #{item.author}" 
+			puts "Item named: #{item.name} \t Made by: #{item.author}"
 		end
-    puts @items_array.length
 	end
 
 	def display_item_by(attribute, value)
 		i = find_item_by(attribute, value)
 		item = @items_array[i]
-		puts "Item named: #{item.name} \t Made by: #{item.author}" 
+		puts "Item named: #{item.name} \t Made by: #{item.author}"
 	end
 
 	def remove_item_by(attribute, value)
 		removable_item = find_item_by(attribute, value)
-
+    puts "Removing item with #{attribute} #{removable_item.send(attribute)}" #send in this case takes a string and sends it as a method to the object
+    @items_array.delete(removable_item)
 	end
 
 	def find_item_by(attribute, value)
 		case attribute
-		when 'name'
+		when :name
 			find_item_by_name(value)
-		when 'author'
+		when :author
 			find_item_by_author(value)
 		else
-			puts "Not a valid query type, please select either: name or author"
+			puts 'Not a valid query type, please select either: name or author'
 		end
 
 		# if attribute ==  "name"
@@ -50,7 +51,7 @@ class Inventory
 	def find_item_by_name(value)
 		@items_array.each_with_index do |item, index|
 			if item.name == value
-				return index
+        return item
 			end
 		end
 		puts "No matches found please check your spelling"
@@ -59,7 +60,7 @@ class Inventory
 	def find_item_by_author(value)
 		@items_array.each_with_index do |item, index|
 			if item.author == value
-				return index
+        return item
 			end
 		end
 		puts "No matches found please check your spelling"
@@ -70,8 +71,8 @@ class Item
 	attr_accessor :name, :author
 
 	def initialize(name, author)
-		@name = name.to_s.upcase!
-		@author = author.to_s.upcase!
+		@name = name.to_s
+		@author = author.to_s
 	end
 
 	# Instead of using attr_accessor, attr_reader, or attr_writer
@@ -103,4 +104,5 @@ my_inventory.add_item("Yolo", "Steinbeck")
 my_inventory.display_inventory
 my_inventory.add_item("Green", "Bluish")
 my_inventory.display_inventory
-#user_input(my_inventory)
+my_inventory.remove_item_by(:name, "Green")
+my_inventory.display_inventory
